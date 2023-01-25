1d75f875665f7e1fa4198bef7eb4c3ffd9bd1a3e
p-sthely <contact@philippesthely.com>
2023-01-25 16:22:38 +0100

    edit Navbar props; implement props in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,29 +5,29 @@ export default function Navbar ( props ) {
       <div class="container-fluid">
         <a class="navbar-brand" href="${ props.brand.href }">${ props.brand.value }</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          ${ props.toggler.icon }
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="${ props.nav[0].href }">${ props.nav[0].value }</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="${ props.nav[1].href }">${ props.nav[1].value }</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
+              <a class="nav-link dropdown-toggle" href="${ props.nav[2].href }" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                ${ props.nav[2].value }
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="${ props.nav[2].items[0].href }">${ props.nav[2].items[0].value }</a></li>
+                <li><a class="dropdown-item" href="${ props.nav[2].items[1].href }">${ props.nav[2].items[1].value }</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="${ props.nav[2].items[2].href }">${ props.nav[2].items[2].value }</a></li>
               </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a class="nav-link disabled">${ props.nav[3].value }</a>
             </li>
           </ul>
           <form class="d-flex" role="search">
--- src/js/props.js
+++ src/js/props.js
@@ -2,6 +2,23 @@ export const NAVBAR = {
   brand: {
     value: 'Navbar',
     href: '#'
-  }
+  },
+  toggler: {
+    icon: '<i class="bi bi-list"></i>'
+  },
+  nav: [
+    { value: 'Home', href: '#' },
+    { value: 'Link', href: '#' },
+    {
+      value: 'Dropdown',
+      href: '#',
+      items: [
+        { value: 'Action', href: '#' },
+        { value: 'Another action', href: '#' },
+        { value: 'Something else here', href: '#' }
+      ]
+    },
+    { value: 'Disabled', href: '' }
+  ]
 }
 
